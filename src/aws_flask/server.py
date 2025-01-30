8105f7baf0c6ab986e33fccee7e167388f460dec
Elai Shalev <eshalev@redhat.com>
2025-01-30 11:29:05 +0200

    added tests

--- src/aws_flask/server.py
+++ src/aws_flask/server.py
@@ -1,27 +1,25 @@
 from flask import Flask
 from flask import request
 
-def create_app():
-  app = Flask(__name__)
+app = Flask(__name__)
 
-  users = []
-  users.append({
-      "name": "Marcin",
-      "country": "Polska",
-      "cat_amount": 3,
-    })
+users = []
+users.append({
+    "name": "Marcin",
+    "country": "Polska",
+    "cat_amount": 3,
+  })
 
-  @app.route("/users", methods=['GET'])
-  def get_user():
-    if request.args.get('latest') == 'true': # return latest user
-      return users[-1]
-    return users # return all users
+@app.route("/users", methods=['GET'])
+def get_user():
+  if request.args.get('latest') == 'true': # return latest user
+    return users[-1]
+  return users # return all users
 
 
-  @app.route("/users", methods=['POST'])
-  def add_user():
-    users.append(request.json)
-    return "New User added successfully."
-  
-  return app 
+@app.route("/users", methods=['POST'])
+def add_user():
+  users.append(request.json)
+  return {"response": "New User added successfully."}
+
 
---
+++ tests/test_server.py
@@ -0,0 +1,41 @@
+from src.aws_flask.server import app 
+import pytest
+
+new_user = {
+    "name": "Elai",
+    "country": "Israel",
+    "cat_amount": 2,
+}
+
+@pytest.fixture
+def client():
+  with app.test_client() as client:
+    yield client
+
+def test_get_latest_user(client):
+  response = client.get('/users?latest=true')
+  assert response.status_code == 200
+  assert response.json == {
+      "name": "Marcin",
+      "country": "Polska",
+      "cat_amount": 3,
+    }
+
+
+def test_add_user(client):
+  response = client.post('/users', json=new_user)
+  assert response.status_code == 200
+  assert response.headers["Content-Type"] == "application/json"
+  assert response.json == {"response": "New User added successfully."}
+
+
+def test_get_all_users(client):
+  response = client.post('/users', json=new_user)
+  assert response.status_code == 200
+  response = client.get('/users')
+  assert response.status_code == 200
+  assert response.json == [{'cat_amount': 3, 'country': 'Polska', 'name': 'Marcin'}, {'cat_amount': 2, 'country': 'Israel', 'name': 'Elai'}, {'cat_amount': 2, 'country': 'Israel', 'name': 'Elai'}]
+
+def test_non_existant(client):
+  response = client.get('/non-existent')
+  assert response.status_code == 404
